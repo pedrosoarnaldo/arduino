a47242c0deda8821580c54a4ed02c17eba7491c7
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2016-11-22 02:23:33 -0200

    Alterado o semaforo.ino

--- semaforo.ino
+++ semaforo.ino
@@ -1,39 +1,20 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-*/
-
-
-// the setup function runs once when you press reset or power the board
+// the setup routine runs once when you press reset:
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize the digital pin as an output.
   pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+// the loop routine runs over and over again forever:
 void loop() {
-
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);                       // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);                       // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);                       // wait for a second
-  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, HIGH);   // turn the RED LED on (HIGH is the voltage level)
+  delay(4000);              // wait 4 seconds
+  digitalWrite(13, LOW);    // turn the  RED LED off by making the voltage LOW
+  digitalWrite(2, HIGH) ;   // turn the Green LED on (HIGH is the voltage level)
+  delay(4000);              // wait 4 seconds
+  digitalWrite(2, LOW);     // turn the Green LED off by making the voltage LOW
+  digitalWrite(8, HIGH) ;   // turn the Yellow LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(8, LOW);     // turn the Yellow LED off by making the voltage LOW
 }
