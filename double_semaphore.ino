62d09cddc906d7a5796e627b41e36176b5abddf2
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2016-11-22 11:48:59 -0200

    Created a double semaphore control

---
+++ double_semaphore.ino
@@ -0,0 +1,55 @@
+#define S1_Red_Led 13
+#define S1_Yellow_Led 12
+#define S1_Green_Led 11
+
+#define S2_Red_Led 7
+#define S2_Yellow_Led 6
+#define S2_Green_Led 5
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(S1_Red_Led, OUTPUT);
+  pinMode(S1_Yellow_Led, OUTPUT);
+  pinMode(S1_Green_Led, OUTPUT);
+  
+  pinMode(S2_Red_Led, OUTPUT);
+  pinMode(S2_Yellow_Led, OUTPUT);
+  pinMode(S2_Green_Led, OUTPUT);
+  
+  digitalWrite(S1_Red_Led, LOW);
+  digitalWrite(S1_Yellow_Led, LOW);
+  digitalWrite(S1_Green_Led, LOW);
+  
+  digitalWrite(S2_Red_Led, LOW);
+  digitalWrite(S2_Yellow_Led, LOW);
+  digitalWrite(S2_Green_Led, LOW);
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  
+  // Control the Semaphore 
+  digitalWrite(S2_Red_Led, LOW);
+  digitalWrite(S1_Red_Led, HIGH);
+  digitalWrite(S2_Green_Led, HIGH);
+  delay(4000);
+  
+  digitalWrite(S2_Green_Led, LOW);
+  digitalWrite(S2_Yellow_Led, HIGH);
+  delay(2000);
+  digitalWrite(S2_Yellow_Led, LOW);
+
+  digitalWrite(S2_Red_Led, HIGH);
+  digitalWrite(S1_Red_Led, LOW);
+  digitalWrite(S1_Green_Led, HIGH);
+  delay(4000);
+  
+  digitalWrite(S1_Green_Led, LOW);
+  digitalWrite(S1_Yellow_Led, HIGH);
+  delay(2000);
+  digitalWrite(S1_Yellow_Led, LOW);
+  
+}
