12a8b25c846aa4365dab566a8007df9ee58cb89f
arnaldo <pedroso.arnaldo@gmail.com>
2021-06-21 17:23:48 -0300

    .

--- automacao_panela/automacao_panela.ino
+++ automacao_panela/automacao_panela.ino
@@ -2,13 +2,30 @@
 #include <DallasTemperature.h>
 
 #define ONE_WIRE_BUS 8
+#define BotaoResistencia 7
+#define BotaoBomba 5
+#define LedResistencia 6
+#define LedBomba 4
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 float Celsus = 0;
 
+int EstadoBotaoResistencia = 0;
+int EstadoBotaoBomba = 0;
+
 void setup() {
   // put your setup code here, to run once:
+
+ pinMode(BotaoResistencia, INPUT);
+ pinMode(BotaoBomba, INPUT);
+
+ pinMode(LedResistencia, OUTPUT);
+ pinMode(LedBomba, OUTPUT);
+
+ digitalWrite(LedResistencia, LOW);
+ digitalWrite(LedBomba, LOW);
+ 
  sensors.begin();
  Serial.begin(9600);
 
@@ -32,9 +49,28 @@ void loop() {
   celsus = getTemperature();
   setTemp = setTemperature();
 
+  if ((digitalRead(BotaoResistencia) == HIGH) && (EstadoBotaoResistencia == 0)) {
+    EstadoBotaoResistencia = 1;
+    digitalWrite(LedResistencia, HIGH);
+  } else {
+    if ((digitalRead(BotaoResistencia) == HIGH) && (EstadoBotaoResistencia == 1)) {
+    digitalWrite(LedResistencia, LOW);
+    EstadoBotaoResistencia = 0;
+    }
+  }
+ 
+  if ((digitalRead(BotaoBomba) == HIGH) && (EstadoBotaoBomba == 0)) {
+    EstadoBotaoBomba = 1;
+    digitalWrite(LedBomba, HIGH);
+  } else {
+    if ((digitalRead(BotaoBomba) == HIGH) && (EstadoBotaoBomba == 1)) {
+    digitalWrite(LedBomba, LOW);
+    EstadoBotaoBomba = 0;
+    }
+  }
+   
   Serial.println(celsus);
   Serial.println(setTemp);
+  delay(100);
   
-  delay(1000);
-
 }
